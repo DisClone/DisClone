a0820a2ae6f2eb82b4dd06879b2dcd1e6b6d27ab
preston <mechampreston@gmail.com>
2016-09-12 09:57:08 -0600

    updated routing, removed uneeded code

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,35 +1,17 @@
 import React from "react";
-import { Link } from "react-router";
-import Group from "../components/Groups/Groups";
-import Navigation from "../components/Navigation/Navigation";
-// import HomeNav from "../components/Home/Home-nav.js";
-// import HomeChat from "../components/Home/Home-chat.js";
-
+import Navigation from './Navigation';
 
 class Layout extends React.Component {
 
   render(){
-      const { location } = this.props;
-      const groupBar = {width:'5rem', height: "100%", color:"#fff", textAlign: "center"};
-      const mainContainer = {width: "100vw", height:"100vh", margin:"0px", backgroundColor:"#1E2124", display:"flex", color:"#fff"};
-    //Links to routed paths
+
+    const mainContainer = {width: "100vw", height:"100vh", margin:"0px", backgroundColor:"#1E2124", display:"flex", color:"#fff"};
+
     return(
-    <div>
         <div style={mainContainer}>
-          <div style={groupBar}>
-            <br />
-            <Link to="/@me">  ME</Link>
-
-            <br /><br /><br /><br /><br />
-
-            <Link to={'/channels/5/'}>Group 5</Link>
-
-          </div>
+            <Navigation />
             {this.props.children}
-
-
         </div>
-      </div>
     );
   }
 }
