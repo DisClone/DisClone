1fe0078a95755e668186113609058db62fd0a4eb
TheTopazWombat <iceman.ike@gmail.com>
2016-09-20 15:46:27 -0600

    Partially broken code, do not push

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -25,15 +25,23 @@ export function loadUser(user) {
   }).then(response => {
     user = response.data;
     console.log(user);
-    user.socket = window.io('http://localhost:3000/');
+    user.socket = window.io('http://localhost:3000/', {query: "user_id=" + user.userData.id});
     for (let i = 0; i < user.friends.length; i++) {
       user.socket.emit('channels', user.friends[i].privateChannel.id);
     }
     for (let i = 0; i < user.groups.length; i++) {
+      user.socket.emit('groups', user.groups[i].id);
       for (let k = 0; k < user.groups[i].channels.length; k++) {
         user.socket.emit('channels', user.groups[i].channels[k].id);
       }
     }
+    user.socket.on('group-users', function(users) {
+      for (let i = 0; i < user.groups.length; i++) {
+        if (user.groups[i].id === users.groupId) {
+          users.groups[i].online_users = users.groupUsers;
+        }
+      }
+    });
     return response.data;
 
   }).then( response => {
